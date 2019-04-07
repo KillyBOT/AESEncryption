4b9de98025fd3ebc3b883068bbe5408131d46051
KillyBOT <edwards.kyle.j@gmail.com>
2019-04-07 15:43:47 -0400

    Finished GF inverse lookup

--- AESEncryption.c
+++ AESEncryption.c
@@ -3,21 +3,41 @@
 #include <string.h>
 
 #define IMMUTABLE_POLYNOMIAL 0x11b
+#define GENERATOR 0x03
+#define C_BYTE 0x63
 
 typedef unsigned char byte;
 
 byte GFMult(byte, byte);
 byte GFExp(byte, byte);
+byte GFFastMult(byte, byte, byte*, byte*);
+byte GFInv(byte, byte*);
 byte* createGFExpTable(byte);
-void printGFExpTable(byte*);
+byte* createGFLogTable(byte);
+byte* createGFInvTable(byte*, byte*);
+void printGFTable(byte*);
 
 int main(){
     byte test1 = 0xb6;
     byte test2 = 0x53;
+    byte* expTable;
+    byte* logTable;
+    byte* invTable;
 
     printf("%x\t%x\n",GFMult(test1, test2), GFExp(0x03,0x02));
 
-    byte* table = createGFExpTable(0x03);
+    printf("%d\n",sizeof(byte));
+
+    expTable = createGFExpTable(GENERATOR);
+    logTable = createGFLogTable(GENERATOR);
+    invTable = createGFInvTable(expTable, logTable);
+    //printGFTable(expTable);
+    //printGFTable(logTable);
+    //printGFTable(invTable);
+
+    free(expTable);
+    free(logTable);
+    free(invTable);
 
     return 0;
 }
@@ -54,28 +74,69 @@ byte GFExp(byte base, byte power){
 }
 
 byte* createGFExpTable(byte generator){
-    byte* table = malloc(sizeof(byte) * sizeof(byte));
-
-    table[0x00] = 0x63;
-
+    byte* table = malloc(256);
+    
     for(byte b = 0; b < 0xff; b++){
-        table[ GFExp(generator,b) ] = b;
-        printf("%x\t%x\n", GFExp(generator, b),b);
+        table[ b ] = GFExp(generator, b);
+        //printf("%x\t%x\n", GFExp(generator, b),b);
     }
 
     return table;
 }
 
-void printGFExpTable(byte* table){
-    printf("  ");
-    for(byte b = 0; b < 16; b++) printf("%x ",b);
+byte* createGFLogTable(byte generator){
+    byte* table = malloc(256);
+
+
+    
+    for(byte b = 0; b < 0xff; b++){
+        table[ GFExp(generator, b) ] = b;
+        //printf("%x\t%x\n", GFExp(generator, b),b);
+    }
+
+    return table;
+}
+
+void printGFTable(byte* table){
+    printf("   ");
+    for(byte b = 0; b < 16; b++) printf("%x  ",b);
     printf("\n");
 
     for(byte row = 0; row < 16; row++){
         printf("%x ", row);
         for(byte col = 0; col < 16; col++){
-            printf("%x ", table[row + col]);
+            if(table[(row << 4) | col] < 0x10) printf("0");
+            printf("%x ", table[(row << 4) | col]);
         }
         printf("\n");
     }
+}
+
+byte GFFastMult(byte a, byte b, byte* ExpTable, byte* LogTable){
+    if(a == 0 || b == 0) return b;
+    int t = LogTable[a] + LogTable[b];
+    if(t > 0xff) t = t & 0xff;
+    return ExpTable[t];
+}
+
+byte* createGFInvTable(byte* expTable, byte* logTable){
+
+    byte* table = malloc(256);
+
+    for(byte b = 1; b < 0xff; b++){
+        table[b] = expTable[0xff - logTable[b]];
+    }
+
+    //Make some adjustments to the table to make it work, probably not good practice
+
+    table[0xff] = expTable[0xff - logTable[0xff]];
+    table[0x01] = 0x01;
+    table[0x00] = C_BYTE;
+
+    return table;
+
+}
+
+byte GFInv(byte in, byte* invTable){
+    return invTable[in];
 }
